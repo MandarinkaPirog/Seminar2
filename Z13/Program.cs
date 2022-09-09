10e53604e16f0fff9756f24ac7c7e3b88cdb26c5
Vika <mandarinkavikabakalova@gmail.com>
2022-09-09 15:40:40 +0400

    Задача 13

---
+++ Z13/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string num2 = Convert.ToString(num1);
+if (num2.Length > 2)
+{
+    Console.Write("Третья цифра:"+ num2[2]);
+}
+
+else
+{
+    Console.Write("Третьей цифры нет");
+}
